2612cb531bffe5ec71c9eb61aea0985bd6164392
Dean McGuigan <deantmcg@gmail.com>
2024-10-04 20:01:42 +0200

    xUnit Test Project and ProductController Unit Tests added

--- Shopfinity.ProductService/obj/Debug/net8.0/Shopfinity.ProductService.AssemblyInfo.cs
+++ Shopfinity.ProductService/obj/Debug/net8.0/Shopfinity.ProductService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopfinity.ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5646432e765de849de84cdd2a0ba8cbf54c75dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed3c82231a1f5d7efd6c03212f4c5292158b27e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopfinity.ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopfinity.ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Shopfinity/Shopfinity.ProductService.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Shopfinity.ProductService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
